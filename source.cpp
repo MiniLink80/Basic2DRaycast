11f04fe600a57d1cd359b9df8fdb89182f8e0fe9
MiniLink80 <akk.eren.ys@gmail.com>
2021-05-05 19:40:30 +0300

    Cleanup

--- source.cpp
+++ source.cpp
@@ -38,6 +38,8 @@ int Pos(float x, float y){
     return w*(int)y+(int)x;
 }
 
+//Be careful not to set the position inside of a wall. 
+//I will not be held responsible for anything that happens if you're not careful.
 float x = 13.68;
 float y = 2.27;
 float angle = -0.77;
@@ -68,7 +70,7 @@ int main(){
 
     screen[Pos(x, y)] = 'O';
 
-    //boilerplate code to set the screen up
+    //Boilerplate code to set the screen up
 	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 	SetConsoleActiveScreenBuffer(hConsole);
 	DWORD dwBytesWritten = 0;
@@ -82,8 +84,7 @@ int main(){
 		chrono::duration<float> deltaTime = tp2 - tp1;
 		tp1 = tp2;
         float dt = deltaTime.count();
-
-        //the past few lines are to calculate deltaTime. It's there to make movement consistent even when FPS changes.
+        //The past few lines are to calculate deltaTime. It's there to make movement consistent even when FPS changes.
 
         if(GetAsyncKeyState('A')){
             angle -= 1.0 * ANGLE_SPEED * dt;
@@ -103,62 +104,72 @@ int main(){
         
 
         float middleRayX, middleRayY;
-        //we cast 120 rays
+        //120 rays are cast.
         for (int a = 0; a < w; a++){
+            //Weird formula stolen from javidx9. This is the angle of the current ray.
             float curA = (angle - FOV/2.0f) + ((float)a / (float)w) * FOV;
             
             bool hitWall = false;
             int stepCount = 0;
             float rayX = x;
             float rayY = y;
+
+            //The ray's position is incremented by a small step each time
+            //to find the precise moment of contact with the wall
             while (!hitWall && stepCount < 1000){
+                
                 rayX += 0.025 * cosf(curA);
                 rayY += 0.025 * sinf(curA);
+                
                 if (map[(int)rayY][(int)rayX] == 1)
                     hitWall = true;
                 else
                     stepCount++;
             }
-            float distance = sqrt((rayX-x)*(rayX-x)+(rayY-y)*(rayY-y));
-            int size = (int)(h/distance)*1.25;
+
             if (a == 60){
                 middleRayX = rayX;
                 middleRayY = rayY;
             }
-                //we fill everything if size is above 119 to prevent crashing
-                if (size > 119){
-                    for (int i = 0; i < h; i++){
-                        screen[Pos(a, i)] = 0x2588;
+
+            float distance = sqrt((rayX-x)*(rayX-x)+(rayY-y)*(rayY-y));
+            int size = (int)(h/distance)*1.25;
+            
+            //Fill everything if size is above 119 to prevent crashing for some unknown reason
+            if (size > 119){
+                for (int i = 0; i < h; i++){
+                    screen[Pos(a, i)] = 0x2588;
+                }
+            }
+            else{
+                //Check if the ray is close to a cube edge and render the edge accordingly
+                if (fabsf(rayX - round(rayX)) > 0.05 || fabsf(rayY - round(rayY)) > 0.05){
+                    for (int i = (int)(h-size)/2; i < (h+size)/2; i++){
+                        //The wall is darker as we get further away from it
+                        int tile;
+                        if (distance < 3.0)	    	tile = 0x2588;
+                        else if (distance < 6.0)	tile = 0x2593;
+                        else if (distance < 12.0)	tile = 0x2592;
+                        else if (distance < 16.0)	tile = 0x2591;
+                        screen[Pos(a, i)] = tile;
                     }
                 }
-                else{
-                    //we check if the ray is close to a cube edge and render the edge accordingly
-                    if (fabsf(rayX - round(rayX)) > 0.05 || fabsf(rayY - round(rayY)) > 0.05){
-                        for (int i = (int)(h-size)/2; i < (h+size)/2; i++){
-                            int tile;
-                            if (distance < 3.0)	    	tile = 0x2588;
-                            else if (distance < 6.0)	tile = 0x2593;
-                            else if (distance < 12.0)	tile = 0x2592;
-                            else if (distance < 16.0)	tile = 0x2591;
-                            screen[Pos(a, i)] = tile;
-                        }
-                    }
 
-                    //empty space if we're close enough
-                    else {
-                        for (int i = (int)(h-size)/2; i < (h+size)/2; i++){
-                            screen[Pos(a, i)] = ' ';
-                        }
-                    }
-
-                    //fill top and bottom
-                    for (int i = 0; i < (int)(h-size)/2; i++){
-                        screen[Pos(a, i)] = ' ';
-                    }
-                    for (int i = (int)(h+size)/2; i < h; i++){
+                //Empty space if the player is close enough
+                else {
+                    for (int i = (int)(h-size)/2; i < (h+size)/2; i++){
                         screen[Pos(a, i)] = ' ';
                     }
                 }
+
+                //Fill floor and ceiling
+                for (int i = 0; i < (int)(h-size)/2; i++){
+                    screen[Pos(a, i)] = ' ';
+                }
+                for (int i = (int)(h+size)/2; i < h; i++){
+                    screen[Pos(a, i)] = ' ';
+                }
+            }
         }   
         
 
@@ -171,12 +182,12 @@ int main(){
                     screen[Pos(j, i)+w] = '.';
             }
         }
+        
         swprintf_s(screen, 70, L"X=%.2f Y=%.2f Angle=%.2f, rX=%.2f rY=%.2f FPS=%.2f", x, y, angle, middleRayX, middleRayY, 1.0f/dt);
         screen[Pos(x,y)+w] = 'O';
         screen[w * h - 1] = '\0';
         WriteConsoleOutputCharacter(hConsole, screen, h*w, {0, 0}, &dwBytesWritten);
     }
-    
     
     return 0;
 }
