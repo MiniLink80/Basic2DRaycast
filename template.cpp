fc32bfe5fae48b07c191d0ccc9db62ae436660ab
MiniLink80 <akk.eren.ys@gmail.com>
2021-05-07 22:18:38 +0300

    Revert "update"
    This reverts commit 04cec238a14ca150318cdf42a971e31ff23bec22.

--- template.cpp
+++ template.cpp
@@ -7,7 +7,6 @@ the screen array inside of the while loop.
 #define UNICODE
 #include <windows.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <chrono>
 
 using namespace std;
@@ -24,7 +23,11 @@ int main(){
     wchar_t *screen = new wchar_t[w*h]; 
     
     //Fill it with spaces first to prevent weird stuff from happening
-    memset(screen, ' ', h*w*sizeof(wchar_t));
+    for (int i = 0; i < h; i++){
+        for (int j = 0; j < w; j++){
+            screen[i*w+j] = ' ';
+        }
+    }
 
     //Set the screen up
 	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
